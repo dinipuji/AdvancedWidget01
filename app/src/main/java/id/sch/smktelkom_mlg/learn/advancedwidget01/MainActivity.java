04325e54598ac013e899b5b9bfb704bbf438f05b
DiniPuji_PenyewaanHotel <dinipuji18@gmail.com>
2016-09-21 10:58:36 +0700

    Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget01/MainActivity.java
@@ -0,0 +1,16 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
